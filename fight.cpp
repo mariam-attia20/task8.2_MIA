4bae834a1d68e38fc872002b8095a55db0b8276e
mariam attia <mariamattia94.75@gmail.com>
2025-08-23 05:13:31 +0200

    basice

---
+++ fight.cpp
@@ -0,0 +1,29 @@
+//libraries to include
+
+
+
+
+//create a class for the characters
+//attributes: health , weapon , name
+//functions ; attack , if they are alive 
+
+
+//class inheritance for eagh character type
+//one for optimuseprime
+//one for megatron
+
+
+//creat a class for weapons
+//attributes: name , damage
+//functions: try to hit 
+
+
+
+
+
+
+
+
+
+
+
